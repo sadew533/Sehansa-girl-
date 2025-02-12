f0298180c03aa6430a0aec676c07af199dea2444
Sadew boy <sadewnethmindya@gmail.com>
2025-02-12 18:51:43 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=y7gmAZZT#eLrijimymsxnnpPJPUC5JodYr-MU2mgCBTnql4tytro' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=7upClaaI#coS2ifvwQJZL0K4jsUUGVpAbKQNhgzav52GI0JagMes' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '9473737373,9363636367' : process.env.SUDO,
